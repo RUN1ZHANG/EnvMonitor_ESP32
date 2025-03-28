1ae590368d337a9340960eb45c92f9e5e6328959
zhangrun <zrun258@icloud.com>
2025-03-28 15:17:40 +0800

    break_to_send

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,105 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <Ticker.h>
 
 #define LIGHT_ADC_PIN            33
 #define SOUND_ADC_PIN            32
 #define DHT_DATA_PIN              18
 //LUX=A/v^B
-int a=1;//LUX转换参数
-int b=2;//LUX转换参数
+int lux_a=1;//LUX转换参数
+int lux_b=2;//LUX转换参数
 int adc_light_value;
 float voltage_light;
 float lux;
 int adc_mic_value;
 float voltage_mic;
 float sound;
-float humidity;
+int humidity;
 float temperature;
-String jsonData;
-const char* ssid = "Ax3000T";
-const char* password = "123zr123";
-const char* serverUrl = "http://192.168.31.95:3232/data"; // 服务器地址（局域网）
-// const char* ssid = "Xiaomi 14";
+sensors_event_t a, g, temp;
+// const char* ssid = "Ax3000T";
 // const char* password = "123zr123";
-// const char* serverUrl = "http://192.168.138.188:3232/data"; // Macbook地址（连接Xiaomi 14）
+// const char* serverUrl = "http://192.168.31.95:3232/data"; // 服务器地址（局域网）
+const char* ssid = "Xiaomi 14";
+const char* password = "123zr123";
+const char* serverUrl = "http://192.168.138.188:3232/data"; // Macbook地址（连接Xiaomi 14）
 DHT dht(DHT_DATA_PIN,DHT11);
 Adafruit_MPU6050 mpu;
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
 
 
+void readSensor(){
+  adc_light_value = analogRead(LIGHT_ADC_PIN);
+  voltage_light = adc_light_value * (3.3/4095.0);
+  lux = lux_a/pow(voltage_light,lux_b);
+  adc_mic_value = analogRead(SOUND_ADC_PIN);
+  voltage_mic = adc_mic_value * (3.3/4095.0);
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  mpu.getEvent(&a, &g, &temp);
+}
+void displayData(){
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Accelerater - m/s^2");
+  display.print(a.acceleration.x, 1);
+  display.print(", ");
+  display.print(a.acceleration.y, 1);
+  display.print(", ");
+  display.print(a.acceleration.z, 1);
+  display.println("");
+  display.println("Gyroscope - rps");
+  display.print(g.gyro.x, 1);
+  display.print(", ");
+  display.print(g.gyro.y, 1);
+  display.print(", ");
+  display.print(g.gyro.z, 1);
+  display.println("");
+  display.print("Lux:");display.print(lux);display.println("");
+  display.print("Sound:");display.print(adc_mic_value);display.println("");
+  display.print("Humidy:");display.print(humidity);display.println("");
+  display.print("Temp:");display.print(temperature);display.println("");
+  display.display();
+}
+String packData(){
+  if (isnan(humidity) || isnan(temperature)) {
+    humidity = 0;
+    temperature = 0;
+  }
+  String jsonData = "{";
+  jsonData += "\"temp\":" + String(temperature) + ",";
+  jsonData += "\"hum\":" + String(humidity) + ",";
+  jsonData += "\"sound\":" + String(voltage_mic) + ",";
+  jsonData += "\"light\":" + String(lux) + ",";
+  jsonData += "\"air_quality\":" + String(20) + ",";
+  jsonData += "\"pir\":" + String(1)+ ",";
+  jsonData += "\"acc\":[" + String(a.acceleration.x) + "," + String(a.acceleration.y) + "," + String(a.acceleration.z) + "]"+ ",";
+  jsonData += "\"gyro\":[" + String(g.gyro.x) + "," + String(g.gyro.y) + "," + String(g.gyro.z) + "]";
+  jsonData += "}";
+  return jsonData;
+}
+void sendData() {
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("WiFi Disconnected");
+    return;
+  }
+  
+    String DataToSend = packData();
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+    Serial.println(DataToSend);
+    int httpResponseCode = http.POST(DataToSend);  
+    http.end();
+    Serial.println(httpResponseCode);
+  
+}
+
+Ticker ticker1(sendData, 5000, 0, MILLIS);
+
 void setup() {
   Serial.begin(115200);
-  //WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   // while (WiFi.status() != WL_CONNECTED) {
   //     delay(1000);
@@ -64,65 +133,17 @@ void setup() {
   display.setRotation(0);
   pinMode(LIGHT_ADC_PIN,INPUT);//ADC光敏电阻
   pinMode(SOUND_ADC_PIN,INPUT);//ADC声敏电阻
+  pinMode(5,OUTPUT);
+  digitalWrite(5,HIGH);
+  pinMode(17,OUTPUT);
+  digitalWrite(17 ,LOW);
   dht.begin();
+  ticker1.start();
 }
+
 
 void loop() {
-  adc_light_value = analogRead(LIGHT_ADC_PIN);
-  voltage_light = adc_light_value * (3.3/4095.0);
-  lux = a/pow(voltage_light,b);
-  adc_mic_value = analogRead(SOUND_ADC_PIN);
-  voltage_mic = adc_mic_value * (3.3/4095.0);
-  humidity = dht.readHumidity();
-  temperature = dht.readTemperature();
-  sensors_event_t a, g, temp;
-  char receivedChar = Serial.read();
-  mpu.getEvent(&a, &g, &temp);
-
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println("Accelerater - m/s^2");
-  display.print(a.acceleration.x, 1);
-  display.print(", ");
-  display.print(a.acceleration.y, 1);
-  display.print(", ");
-  display.print(a.acceleration.z, 1);
-  display.println("");
-  display.println("Gyroscope - rps");
-  display.print(g.gyro.x, 1);
-  display.print(", ");
-  display.print(g.gyro.y, 1);
-  display.print(", ");
-  display.print(g.gyro.z, 1);
-  display.println("");
-  display.print("Lux:");display.print(lux);display.println("");
-  display.print("Sound:");display.print(voltage_mic);display.println("");
-  display.print("Humidy:");display.print(humidity);display.println("");
-  display.print("Temp:");display.print(temperature);display.println("");
-  display.display();
-  delay(100);
-  if (isnan(humidity) || isnan(temperature)) {
-    humidity = 0;
-    temperature = 0;
-  }
-  jsonData = "{";
-  jsonData += "\"temp\":" + String(temperature) + ",";
-  jsonData += "\"hum\":" + String(humidity) + ",";
-  jsonData += "\"sound\":" + String(voltage_mic) + ",";
-  jsonData += "\"light\":" + String(lux) + ",";
-  jsonData += "\"air_quality\":" + String(20) + ",";
-  jsonData += "\"pir\":" + String(1)+ ",";
-  jsonData += "\"acc\":[" + String(a.acceleration.x) + "," + String(a.acceleration.y) + "," + String(a.acceleration.z) + "]"+ ",";
-  jsonData += "\"gyro\":[" + String(g.gyro.x) + "," + String(g.gyro.y) + "," + String(g.gyro.z) + "]";
-  jsonData += "}";
-  if(WiFi.status() == WL_CONNECTED){
-    HTTPClient http;
-    http.begin(serverUrl);
-    http.addHeader("Content-Type", "application/json");
-    Serial.println(jsonData);
-    int httpResponseCode = http.POST(jsonData);  
-    http.end();
-    Serial.println(httpResponseCode);
-    delay(1000);
-}
+  readSensor();
+  displayData();
+  ticker1.update();
 }
